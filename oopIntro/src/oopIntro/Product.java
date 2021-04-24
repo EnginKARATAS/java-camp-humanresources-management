d0d9c5da79d427cd9dda45af504a073b3e8c2b33
Engin Karataş <enginkaratas99@gmail.com>
2021-04-24 23:32:18 +0300

    2. ara öncesi

--- oopIntro/src/oopIntro/Product.java
+++ oopIntro/src/oopIntro/Product.java
@@ -1,5 +1,21 @@
 package oopIntro;
 
 public class Product {
-
+	
+	public Product() {
+		System.out.println("Ben çalýþtým");
+	}
+	public Product(int id ,String name, String detail,  double unitPrice) {
+		System.out.println("çok parametreli constuctor");
+		this.id = id;
+		this.name = name;
+		this.detail = detail;
+		this.unitPrice = unitPrice;
+	}
+	
+	int id; 
+	String name;
+	double unitPrice;
+	String detail;
+	
 }
