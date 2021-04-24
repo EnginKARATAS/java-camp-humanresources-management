49d7df35457ead01e851f8fca9e026df12d8d3fe
Engin Karataş <enginkaratas99@gmail.com>
2021-04-25 00:28:43 +0300

    🔴 class, functions, classes .camp day 2 end

---
+++ oopIntro/src/oopIntro/ProductManager.java
@@ -0,0 +1,8 @@
+package oopIntro;
+
+public class ProductManager {
+	//iþ sýnýfý na iþ fonku yazalým //cart=sepet
+	public void addToCart(Product product) {
+		System.out.println("sepete eklendi :" + product.name);
+	}
+}
